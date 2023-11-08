84f5581993b50550453b7a97fc862f98bd241eaf
Ritik Sharma <sharmaritik2002@gmail.com>
2023-11-09 01:59:24 +0530

    initial-commit

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axios_api = axios.create({
-    baseURL: `http://localhost:5000/api/users`,
+    baseURL: `https://heliverse-user-management-server.onrender.com`,
     timeout: 5000
 })
 
